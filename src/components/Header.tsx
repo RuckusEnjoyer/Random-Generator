b58cff35eb3535dea6c015405e54fcbe8cce338f
Kork <hail3yp@gmail.com>
2024-04-18 12:58:35 -0400

    Changing filing system to use Vite + React, changing JS to TS

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+
+function Header() {
+
+    return(
+        <header>
+        <img src="./re.png" />
+        <h2>
+            A random collection of random generators
+        </h2>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/components/Random.tsx
@@ -0,0 +1,16 @@
+function Random() {
+
+    return (
+        <section>
+            <h2>
+                Random Password Generator:
+            </h2>
+            <button id="pass-btn">
+                Click Me
+            </button>
+            <p id="pass-output"></p>
+        </section>
+    )
+}
+
+export default Random
